0440208a1cac667cee41cac2afd9f974ab857960
Yakko Majuri <yakko.majuri@gmail.com>
2020-03-27 07:24:25 -0300

    improvements

--- bot.js
+++ bot.js
@@ -27,9 +27,6 @@ function handleButton() {
     var results = [0, 2, 3];
     if (responseArea.innerHTML == "" && predictions.innerHTML == "") {
         for (var i = 0; i < results.length; i++) {
-            if (i == 6) {
-                break;
-            }
             reply += '<p class="prediction-txt"><a class="predictions" onclick="selectedQuestion(' + results[i] + ')">' + questions[results[i]][2] + '</a></p>';
             if (i == results.length - 1) {
                 predictions.innerHTML = reply;
@@ -51,6 +48,10 @@ function printOptions(res) {
     }
     var html = "";
     for (var i = 0; i < results.length; i++) {
+        if (i == 5) {
+            predictions.innerHTML = html;
+            break;
+        }
         html += '<p class="prediction-txt"><a class="predictions" onclick="selectedQuestion(' + results[i] + ')">' + questions[results[i]][2] + '</a></p>'
         if (i == results.length - 1) {
             predictions.innerHTML = html;
